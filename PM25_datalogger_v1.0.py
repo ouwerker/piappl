476380825685a5d5595828d89be7fa6708b12268
Martin Ouwerkerk <136217766+ouwerker@users.noreply.github.com>
2023-10-06 11:33:14 +0200

    Add files via upload

---
+++ PM25_datalogger_v1.0.py
@@ -0,0 +1,197 @@
+# Author: Martin Ouwerkerk based on Adafruit_CircuitPython_PM25 driver
+# Version 1.0 20231006 
+# License: MIT
+
+import time
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+import board
+
+# Start of sensor specific part
+PM25UNIT = 'ug/m3'
+SENSORNAME = 'PM25'
+SENSORMODALITY = 'particulate matter'
+
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+
+reset_pin = None
+
+
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+# Create library object, use 'slow' 100KHz frequency!
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+# Connect to a PM2.5 sensor over I2C
+pm25 = PM25_I2C(i2c, reset_pin)
+
+print("Found PM2.5 sensor, reading data...")
+
+# End of sensor specific part
+
+# specify timeunit
+TIMEUNIT = 's'
+
+
+# Generate data file name and file path
+import datetime
+import os.path
+save_path='/home/ouwerker/Data/' + SENSORNAME + '/'
+complete_path=os.path.expanduser(save_path)
+if not os.path.exists(complete_path):
+    os.makedirs(complete_path)
+    print("Directory '%s' created successfully" %SENSORNAME)
+
+DATUM=str(datetime.date.today())
+filenr = 1
+filenrstr = str(filenr)
+FILENUMBER='_0'+filenrstr
+name_of_file=SENSORNAME + 'output_'
+name_of_file=name_of_file+DATUM+FILENUMBER
+complete_name=os.path.join(complete_path,name_of_file+'.csv')
+EXISTS=os.path.exists(complete_name)
+while  EXISTS :
+    filenr=filenr+1
+    filenrstr=str(filenr)
+    if filenr < 10 :
+        FILENUMBER='_0'+filenrstr
+    else :
+        FILENUMBER='_'+filenrstr
+    name_of_file = SENSORNAME +'output_'
+    name_of_file=name_of_file+DATUM+FILENUMBER
+    complete_name=os.path.join(complete_path,name_of_file+'.csv')
+    EXISTS=os.path.exists(complete_name)
+print(complete_name)
+
+# Choose measurement interval 
+INTERVAL = input('please enter interval in milliseconds :\n')
+INTERVAL=float(INTERVAL)
+while INTERVAL < 1 :
+    INTERVAL = input('value out of range, please enter a positive interval value of 1 or more:\n')
+    INTERVAL = float(INTERVAL)
+INTERVAL=INTERVAL/1000
+
+# Choose duration of data collection
+DURATION = input('please enter duration in seconds :\n')
+DURATION = int(DURATION)
+while DURATION < 1 :
+    DURATION = input('invalid entry, please enter a duration value of 1 or more :\n')
+    DURATION = int(DURATION) 
+
+# Choose real time graph of data or only at end of data collection
+REALTIMEGRAPH = input('real time graph? Y/N  ')
+
+# set plot parameters
+PLOTTITLE = SENSORNAME + ' ' + SENSORMODALITY + ' sensor'
+fig, ax=plt.subplots(figsize=(7,4.5))
+ax.set_title(PLOTTITLE)
+ax.set_xlabel('time ['+ TIMEUNIT + ']')
+
+PLOTYLABEL = 'PM25 [' + PM25UNIT + ']'
+
+ax.set_ylabel(PLOTYLABEL)
+
+
+# Write settings to csv file
+line_count = 0
+from datetime import datetime
+STARTTIME = datetime.now().time()
+
+with open(complete_name,'w', newline='') as csvfile:
+    fieldnames = ['startdate', 'starttime']
+    line_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    line_writer.writeheader()
+    line_writer.writerow({'startdate': DATUM, 'starttime': STARTTIME})
+    line_count += 1
+
+with open(complete_name,'a', newline='') as csvfile:
+    fieldnames = ['sensorname', 'pm25_unit', 'duration', 'timeunit']
+    line_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    line_writer.writeheader()
+    line_writer.writerow({'sensorname': SENSORNAME, 'pm25_unit': PM25UNIT, 'duration': DURATION, 'timeunit': TIMEUNIT})
+    line_count += 1
+
+print(f'{datetime.now().time()}\n')
+print(f'Reading {SENSORNAME} for {DURATION:6d} seconds')
+start = time.time()
+
+import array as arr
+TIMEARRAY=arr.array('f')
+PM25ARRAY=arr.array('f')
+
+# Write header to csv file
+with open(complete_name,'a', newline='') as csvfile:
+    fieldnames = ['time', PM25UNIT]
+    line_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    line_writer.writeheader()
+    line_count += 1 
+
+while (time.time() - start) <= DURATION:
+    
+   
+    time.sleep(1)
+
+    try:
+        aqdata = pm25.read()
+        # print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+
+    print()
+    print("Concentration Units (standard)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    )
+    print("Concentration Units (environmental)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    )
+    print("---------------------------------------")
+    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+    print("---------------------------------------")
+
+    
+    # Start of sensor specific part: read the sensor value and print it out.
+    PM25 = aqdata["pm25 env"]
+    # End of sensor specific part
+    PM25ARRAY.extend([PM25])
+    voortgang=time.time()-start
+    TIMEARRAY.extend([voortgang])
+ 
+    # Write sensor output to csv file
+    with open(complete_name,'a', newline='') as csvfile:
+        fieldnames = ['time', PM25UNIT]
+        line_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        line_writer.writerow({'time': voortgang, PM25UNIT:PM25})
+        line_count += 1
+        
+    print(f'Seconds since start {voortgang:.3f} sensor {SENSORNAME} reading {PM25:3d} {PM25UNIT}')
+ 
+    ax.scatter(TIMEARRAY,PM25ARRAY,marker='.', color='blue')
+    # Show real time graph during interval
+    if REALTIMEGRAPH == 'Y' or REALTIMEGRAPH == 'y': plt.pause(INTERVAL)
+    else : time.sleep(INTERVAL)
+    
+# Show graph with end result
+plt.show()
+plt.clf()
+
+# Wrapping up
+csvfile.close()
+
